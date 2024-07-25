a866de94135c2cd6dd8882edb85cfe1669b7cba8
Italo Fialho <italofialho@hotmail.com.br>
2024-07-25 00:11:49 -0300

    wip: landing page

--- src/components/animatedNumber/index.tsx
+++ src/components/animatedNumber/index.tsx
@@ -1,17 +1,39 @@
 "use client";
+import { useEffect, useState } from "react";
+
 import CountUp from "react-countup";
 
 import { AnimatedNumberProps } from "./interface";
 
 export function AnimatedNumber(props: Readonly<AnimatedNumberProps>) {
+  const [isServer, setIsServer] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsServer(false);
+    }, 1_000);
+  }, []);
+
+  if (typeof window === "undefined" || isServer)
+    return (
+      <>
+        {props.$prefix}
+        {props.$startValue ?? props.$value}
+        {props.$suffix}
+      </>
+    );
+
+  return <AnimatedNumberClientSide {...props} />;
+}
+
+function AnimatedNumberClientSide(props: Readonly<AnimatedNumberProps>) {
   return (
     <CountUp
-      start={props.$startValue}
+      start={props.$startValue ?? 1}
       end={props.$value}
       duration={2}
       prefix={props.$prefix}
       suffix={props.$suffix}
-      startOnMount
     />
   );
 }
